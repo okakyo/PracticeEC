80bc12cb68c6f03e80b6d73d01e30f2bf7e65d04
Kyohei Oka <0622okakyo@gmail.com>
2019-03-27 12:07:07 +0900

    '今後の修正必要'

---
+++ EC/src/app/home/shopitem.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http,Headers } from '@angular/http';
+import { Item } from '../models/item.model';
+import 'rxjs/add/operator/toPromise';
+@Injectable()
+export class ShopItem {
+    item:Item[]=[];
+    private Url='';
+    private headers =new Headers({'Content-Type':'application/json'});
+
+    constructor(private http:Http){}
+
+    getAllItem():Promise<Item[]>{
+        return this.http.get(this.Url)
+        .toPromise()
+        .then(res=>res.json() as Item[])        
+    }
+}
